da5935867ad2a24e541dcc6c4b01f6b967ff26fe
Hafsa <nusiamahassan@gmail.com>
2025-06-28 10:37:02 +0300

    dhalashadeyda

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string name = "Hafsa Ali Hassan";
+
+Console.WriteLine("Name: " + name);
+string mothername = "Muhubo Mohamed";
+Console.WriteLine("Momother'Name: " + mothername);
+string dop = "2003-04-25";
+Console.WriteLine("Date Of Birth: " + dop);
+string pob = "Galkaio";
+Console.WriteLine("Place Of Pirt: " + pob);
+string address = "Garsoor";
+Console.WriteLine("Address: " + address);
+int telphone = 0907298143;
+Console.WriteLine("Telphone: " + telphone);
+string sex = "Female";
+Console.WriteLine("Sex: " + sex);
+string status = "Single";
+Console.WriteLine("Status: " + status);
+
